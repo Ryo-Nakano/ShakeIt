08b063b4384d85d4ddf917081bfb047d9256fc23
Create Unity Project <matsuhara9999ryo@yahoo.co.jp>
2018-05-02 14:19:34 +0900

    Initial Commit

---
+++ ShakeIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShakeIt
+//
+//  Created by Ryohei Nanano on 2018/05/02.
+//  Copyright © 2018年 GeekSalon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
